9adf7ead91242e33ed78fd9b7beb221c5b29032f
Said20222 <konodio32@gmail.com>
2025-08-13 14:52:53 +0500

    feat: add admin role for development in Program.cs

--- InventoryHub.Web/Program.cs
+++ InventoryHub.Web/Program.cs
@@ -19,6 +19,31 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+// TODO: Remove this in production
+using (var scope = app.Services.CreateScope())
+{
+    var roleMgr = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    const string adminRole = "Admin";
+    if (!await roleMgr.RoleExistsAsync(adminRole))
+        await roleMgr.CreateAsync(new IdentityRole(adminRole));
+
+    var adminEmail = "admin@mail.com";
+    var adminPwd = "Admin!12345";
+
+    var admin = await userMgr.FindByEmailAsync(adminEmail);
+    if (admin == null)
+    {
+        admin = new IdentityUser { UserName = adminEmail, Email = adminEmail, EmailConfirmed = true };
+        await userMgr.CreateAsync(admin, adminPwd);
+    }
+
+    if (!await userMgr.IsInRoleAsync(admin, adminRole))
+        await userMgr.AddToRoleAsync(admin, adminRole);
+
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
